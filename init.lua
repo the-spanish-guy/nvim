863bb0a61308b78bed6eac25a69fabf463e26bed
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-01-09 11:36:46 -0300

    chore: add telescope for find files

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -17,11 +18,19 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+      'nvim-telescope/telescope.nvim', tag = '0.1.5',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    }
   }
 local opts = {}
 
 require("lazy").setup(plugins, opts)
+
+local telescopeBuiltin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', telescopeBuiltin.find_files, {})
+vim.keymap.set('n', '<leader>fg', telescopeBuiltin.live_grep, {})
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
