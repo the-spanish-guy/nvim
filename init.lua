68e2d2943a44721f761ef7cda3176ef4b8844b37
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-01-09 18:11:00 -0300

    chore: :recycle: refactor structure

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -17,7 +13,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
+require("settings")
 require("lazy").setup("plugins")
 
 
---
+++ lua/settings/config.lua
@@ -0,0 +1,7 @@
+vim.cmd("set t_Co=256")
+vim.cmd("set encoding=UTF-8")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
