7ea9ce291043cd191ff02984bc4119e66c3a9cf3
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-01-09 20:25:02 -0300

    chore: :lipstick: formatting

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,26 +1,25 @@
 return {
-  {
-      'nvim-telescope/telescope.nvim', tag = '0.1.5',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      config = function()
-        local telescopeBuiltin = require("telescope.builtin")
-        vim.keymap.set('n', '<C-p>', telescopeBuiltin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', telescopeBuiltin.live_grep, {})
-      end
-  },
-  {
-    'nvim-telescope/telescope-ui-select.nvim',
-    config = function()
-      require("telescope").setup {
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
-      }
-      require("telescope").load_extension("ui-select")
-    end
-  }
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local telescopeBuiltin = require("telescope.builtin")
+			vim.keymap.set("n", "<C-p>", telescopeBuiltin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", telescopeBuiltin.live_grep, {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
-
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
-return  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-config = function()
-  local treesitterConfigs = require("nvim-treesitter.configs")
-treesitterConfigs.setup({
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "typescript", "javascript", "html" },
-          sync_install = true,
-          highlight = { enable = true },
-          indent = { enable = true },
-})
-  end
-
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local treesitterConfigs = require("nvim-treesitter.configs")
+		treesitterConfigs.setup({
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "typescript", "javascript", "html" },
+			sync_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
