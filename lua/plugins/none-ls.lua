ac82a8758799c049a1ba6052379bd377e7234519
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-01-09 20:53:30 -0300

    feat :wrench: automatic install formatters and linters
    - add package mason-null-ls
    - add automatic isntall formatters and linters

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,14 +1,32 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.diagnostics.eslint,
-			},
-		})
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.diagnostics.eslint_d,
+				},
+			})
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("mason-null-ls").setup({
+				-- ensure_installed = { "stylua", "prettier" },
+				ensure_installed = nil,
+				automatic_installation = true,
+			})
+		end,
+	},
 }
