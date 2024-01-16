21223ead60bfa54f527d6ad5dda78101a218ace0
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-01-16 14:35:45 -0300

    :recycle: chore(lsp-config): update configs

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,28 +1,73 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.tsserver.setup({ capabilities = capabilities })
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    { "williamboman/mason.nvim", config = true },
+    "williamboman/mason-lspconfig.nvim",
+    "b0o/schemastore.nvim",
+  },
+  config = function()
+    require("mason").setup({
+      ui = {
+        border = "rounded",
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+    require("mason-lspconfig").setup({
+      ensure_installed = vim.tbl_keys(require("plugins.lsp.servers")),
+    })
+
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+    local lspconfig = require("lspconfig")
+    local mason_lspconfig = require("mason-lspconfig")
+    local icons = require("utils.icons")
+
+    mason_lspconfig.setup_handlers({
+      function(server_name)
+        lspconfig[server_name].setup({
+          capabilities = capabilities,
+          on_attach = require("plugins.lsp.on_attach").on_attach,
+          settings = require("plugins.lsp.servers")[server_name],
+          filetypes = (require("plugins.lsp.servers")[server_name] or {}).filetypes,
+        })
+      end,
+    })
+
+    vim.diagnostic.config({
+      title = false,
+      underline = true,
+      virtual_text = true,
+      signs = true,
+      update_in_insert = false,
+      severity_sort = true,
+      float = {
+        source = "always",
+        style = "minimal",
+        border = "rounded",
+        header = "",
+        prefix = "",
+      },
+    })
+
+    local signs = {
+      Hint = "" .. icons.diagnostics.Hint .. "",
+      Error = "" .. icons.diagnostics.Error .. "",
+      Warn = "" .. icons.diagnostics.Warning .. "",
+      Info = "" .. icons.diagnostics.Information .. "",
+    }
+
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end
+
+    -- vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+    -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+  end,
 }
---
+++ lua/plugins/lsp/servers.lua
@@ -0,0 +1,37 @@
+return {
+	jsonls = {
+		settings = {
+			json = {
+				schema = require("schemastore").json.schemas(),
+				validate = { enable = true },
+			},
+		},
+	},
+	lua_ls = {
+		Lua = {
+			telemetry = { enable = false },
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				-- make language server aware of runtime files
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
+				},
+			},
+		},
+	},
+	bashls = {
+		filetypes = { "sh", "zsh" },
+	},
+	vimls = {
+		filetypes = { "vim" },
+	},
+	tsserver = {},
+	-- golangci_lint_ls = {},
+	yamlls = {
+		cmd = { "yaml-language-server", "--stdio" },
+		filetypes = { "yaml" },
+	},
+}
