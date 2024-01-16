ced6c21d78726af2020e006a85443a19927d805f
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-01-16 19:54:51 -0300

    chore: config theme syntax

--- lua/plugins/cattpuccin.lua
+++ lua/plugins/cattpuccin.lua
@@ -3,6 +3,14 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
+    require("catppuccin").setup({
+      flavour = "mocha",
+      integrations = {
+        cmp = true,
+        treesitter = true,
+        neotree = true
+      }
+    })
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }
