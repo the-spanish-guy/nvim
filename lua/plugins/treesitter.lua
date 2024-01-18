a59e495e6f9fe1d6bbb0b40f8f6912965bc1a0b4
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-01-18 20:10:35 -0300

    :rotating_light: chore: run linter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local treesitterConfigs = require("nvim-treesitter.configs")
-		treesitterConfigs.setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "typescript", "javascript", "html" },
-			sync_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-      auto_install = true
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local treesitterConfigs = require("nvim-treesitter.configs")
+    treesitterConfigs.setup({
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "typescript", "javascript", "html" },
+      sync_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      auto_install = true,
+    })
+  end,
 }
