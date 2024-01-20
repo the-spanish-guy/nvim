a49868f54f028a3bdead54e284f2fc291c856fef
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-01-20 01:05:47 -0300

    :children_crossing: chore: fixed some issues and add icons
    - fixed issue `folow_current_file`
    - add icons for git events
    - add folder/files icons

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,25 +6,60 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
+  keys = {
+    { "<C-b>", ":Neotree<CR>", silent = true, desc = "open file explorer" },
+  },
   config = function()
     require("neo-tree").setup({
+      default_component_configs = {
+        indent = {
+          with_markers = true,
+          with_expanders = true,
+        },
+        modified = {
+          symbol = " ",
+          highlight = "NeoTreeModified",
+        },
+        icon = {
+          folder_closed = "",
+          folder_open = "",
+          folder_empty = "",
+          folder_empty_open = "",
+        },
+        git_status = {
+          symbols = {
+            -- Change type
+            added = "",
+            deleted = "",
+            modified = "",
+            renamed = "",
+            -- Status type
+            untracked = "",
+            ignored = "",
+            unstaged = "",
+            staged = "",
+            conflict = "",
+          },
+        },
+      },
+
       window = {
         position = "right",
         mappings = {
           ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
         },
-        filesystem = {
-          follow_current_file = {
-            enabled = true,
-          },
+      },
+      filesystem = {
+        follow_current_file = {
+          enabled = true,
         },
-        buffers = {
-          follow_current_file = {
-            enabled = true,
-          },
+      },
+      buffers = {
+        follow_current_file = {
+          enabled = true,
         },
       },
     })
-    vim.keymap.set("n", "<C-b>", ":Neotree<CR>")
+    -- vim.keymap.set("n", "<C-b>", ":Neotree<CR>")
   end,
 }
