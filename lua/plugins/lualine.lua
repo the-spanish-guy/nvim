7053939fa36cb2daa9074a24d49166ff15437409
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-01-20 01:30:48 -0300

    :children_crossing: chore: improve lualine integration
    - add Lazy package update status to lualine
    - customize color for update visualization

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,10 +2,26 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
+    local lazy_status = require("lazy.status")
+    local mocha = require("catppuccin.palettes").get_palette("mocha")
     require("lualine").setup({
       options = {
         theme = "catppuccin",
+        icons_enabled = true
       },
+      sections = {
+        lualine_x = {
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+            color = { fg = mocha.flamingo },
+          },
+          { "encoding" },
+          { "fileformat" },
+          { "filetype" },
+        },
+      },
+      extensions = { "neo-tree" }
     })
   end,
 }
