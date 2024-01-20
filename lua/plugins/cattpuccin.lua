6e6aac1e2264c04c8dbef42a760254d9644086fa
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-01-19 22:05:00 -0300

    :art: chore: integrate catppuccin theme with other plugins

--- lua/plugins/cattpuccin.lua
+++ lua/plugins/cattpuccin.lua
@@ -10,6 +10,13 @@ return {
         treesitter = true,
         neotree = true,
       },
+      transparent_background = false,
+      navic = {
+        enabled = true,
+        custom_bg = "NONE", -- "lualine" will set background to mantle
+      },
+      fidget = true,
+      mason = true
     })
     vim.cmd.colorscheme("catppuccin")
   end,
--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -4,6 +4,11 @@ return {
   enabled = true,
   config = function()
     require("fidget").setup({
+      notification = {
+        window = {
+          winblend = 0,
+        },
+      },
       window = { blend = 0 },
     })
   end,
