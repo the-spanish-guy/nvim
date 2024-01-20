4d1883b9c50f7fdef9d53a3a92a24601da94d9cd
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-01-20 02:09:53 -0300

    :technologist: chore: get theme dynamically and set colors for lazyupdates

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,27 +1,30 @@
+
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local lazy_status = require("lazy.status")
-    local mocha = require("catppuccin.palettes").get_palette("mocha")
+    local themeName = require("utils.split")
+    local theme = require("catppuccin.palettes").get_palette(themeName)
+
     require("lualine").setup({
       options = {
         theme = "catppuccin",
-        icons_enabled = true
+        icons_enabled = true,
       },
       sections = {
         lualine_x = {
           {
             lazy_status.updates,
             cond = lazy_status.has_updates,
-            color = { fg = mocha.flamingo },
+            color = { fg = theme.flamingo },
           },
           { "encoding" },
           { "fileformat" },
           { "filetype" },
         },
       },
-      extensions = { "neo-tree" }
+      extensions = { "neo-tree" },
     })
   end,
 }
