6613a6f2c15e78e2aee774a7d5792b2740691855
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-02-05 22:15:52 -0300

    :package: chore(package): add markdown-preview.nvim

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,20 @@
+-- install with yarn or npm
+-- install without yarn or npm
+--[[{
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+}]]
+
+return
+-- install with yarn or npm
+{
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+}
