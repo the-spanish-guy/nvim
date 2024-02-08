f7f0177b5f6adf8915f524d82a9ef302d111610c
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-02-07 21:51:59 -0300

    :technologist: chore: add markdown config

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,4 +1,7 @@
 -- install with yarn or npm
+-- install with yarn or npm
+-- install with yarn or npm
+-- install with yarn or npm
 -- install without yarn or npm
 --[[{
     "iamcco/markdown-preview.nvim",
@@ -15,6 +18,13 @@ return
   build = "cd app && yarn install",
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
+    vim.g.mkdp_refresh_slow = 0
+
+    vim.g.mkdp_auto_start = 1
+
+    vim.g.mkdp_auto_close = 1
+
+    vim.g.mkdp_page_title = "「${name}」"
   end,
   ft = { "markdown" },
 }
