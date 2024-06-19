b796e63941d9865d2e3f47cdcfc4a17b4e37887a
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-06-18 22:33:59 -0300

    :technologist: chore: improve telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,8 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    -- cmd = "Telescope",
+    tag = "0.1.6",
+    cmd = "Telescope",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope-ui-select.nvim",
@@ -22,7 +22,7 @@ return {
         return string.format("%s\t\t%s", tail, parent)
       end
 
-      require("telescope").setup({
+      telescope.setup({
         defaults = {
           mappings = {
             i = {
