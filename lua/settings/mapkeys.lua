1db237a9781628d929623b3b7324fe4d549d13ff
Luis Enrique Meza Paredes <luis.paredes@nomadglobal.com>
2025-05-31 16:11:45 -0300

    :sparkles: feat(keymaps): adiciona novos atalhos de teclado
    Adiciona e melhora os atalhos de teclado para melhor produtividade:
    - Navegação entre buffers com Shift+h/l
    - Salvamento rápido com Ctrl+s em modo normal e insert
    - Gerenciamento de janelas divididas com leader+sv/sh
    - Navegação entre janelas com Ctrl+h/j/k/l
    - Renomeação de símbolos com F2 (estilo VSCode)
    - Visualização de referências com Ctrl+F2
    
    Co-authored-by: the-spanish-guy <luyo.lemp@gmail.com>

--- lua/settings/mapkeys.lua
+++ lua/settings/mapkeys.lua
@@ -8,6 +8,33 @@ keymap("n", "<C-a>", "ggvG$", "Seleciona Todo o arquivo")
 -- ir para a linha
 keymap("n", "<C-g>", ":", "Ir para a linha")
 
+-- Navegação entre buffers
+keymap("n", "<S-l>", ":bnext<CR>", "Próximo buffer")
+keymap("n", "<S-h>", ":bprevious<CR>", "Buffer anterior")
+
+-- Salvar com Ctrl+S
+keymap("n", "<C-s>", ":w<CR>", "Salvar arquivo")
+keymap("i", "<C-s>", "<Esc>:w<CR>a", "Salvar arquivo")
+
+-- Fechar buffer
+keymap("n", "<leader>x", ":bd<CR>", "Fechar buffer")
+
+-- Dividir janela
+keymap("n", "<leader>sv", ":vsplit<CR>", "Dividir verticalmente")
+keymap("n", "<leader>sh", ":split<CR>", "Dividir horizontalmente")
+
+-- Navegação entre janelas
+keymap("n", "<C-h>", "<C-w>h", "Janela à esquerda")
+keymap("n", "<C-j>", "<C-w>j", "Janela abaixo")
+keymap("n", "<C-k>", "<C-w>k", "Janela acima")
+keymap("n", "<C-l>", "<C-w>l", "Janela à direita")
+
+-- LSP Renomeação (estilo VSCode)
+-- F2: Renomeia o símbolo sob o cursor
+keymap("n", "<F2>", vim.lsp.buf.rename, "Renomear símbolo")
+-- Ctrl+F2: Mostra todas as referências do símbolo
+keymap("n", "<C-F2>", vim.lsp.buf.references, "Mostrar todas as referências")
+
 --[[
 Movendo a linha selecionada ↑↓
 Alt - j Move a linha do cursor pra cima
