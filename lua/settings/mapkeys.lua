dd09c32e56fd42cad70167bd7791836384216785
Luis Enrique Meza Paredes <luis.paredes@nomadglobal.com>
2025-06-01 00:14:04 -0300

    :recycle: refactor(keymaps): reorganiza atalhos de teclado por contexto
    - Agrupa atalhos em seções lógicas para melhor organização:
      - Edição básica (seleção, salvar, deletar)
      - Navegação (ir para linha, gerenciamento de buffers)
      - Gerenciamento de janelas (divisões, navegação)
      - LSP e código (renomear, definições, diagnósticos)
      - Workspace (gerenciamento de pastas)
      - Movimentação de linhas (mover linhas cima/baixo)
      - Plugins (cliente Kulala)
    
    - Preserva comentários importantes de documentação
    - Adiciona cabeçalhos de seção para melhor legibilidade
    - Mantém referência original da lista quickfix do LSP
    - Mantém instruções detalhadas de movimentação
    
    Co-authored-by: The Spanish Guy <luyo.lemp@gmail.com>

--- lua/settings/mapkeys.lua
+++ lua/settings/mapkeys.lua
@@ -1,24 +1,34 @@
 local keymap = require("utils.keymap")
 
--- { "n", "v" }
-
+--[[ ATALHOS BÁSICOS DE EDIÇÃO ]]
 -- Selecionar tudo
 keymap("n", "<C-a>", "ggvG$", "Seleciona Todo o arquivo")
-
--- ir para a linha
-keymap("n", "<C-g>", ":", "Ir para a linha")
-
--- Navegação entre buffers
-keymap("n", "<S-l>", ":bnext<CR>", "Próximo buffer")
-keymap("n", "<S-h>", ":bprevious<CR>", "Buffer anterior")
 
 -- Salvar com Ctrl+S
 keymap("n", "<C-s>", ":w<CR>", "Salvar arquivo")
 keymap("i", "<C-s>", "<Esc>:w<CR>a", "Salvar arquivo")
 
+--[[
+Deletar linha sem copiar (estilo VSCode)
+"_dd -> onde:
+- "_ : envia a operação para o registro blackhole (nada é copiado)
+- dd : deleta a linha inteira
+Dessa forma, a linha é deletada sem afetar o registro de cópia,
+exatamente como o atalho Ctrl+Shift+K do VSCode
+]]
+keymap("n", "<C-S-k>", '"_dd', "Deletar linha sem copiar")
+
+--[[ NAVEGAÇÃO ]]
+-- Ir para a linha
+keymap("n", "<C-g>", ":", "Ir para a linha")
+
+-- Navegação entre buffers
+keymap("n", "<S-l>", ":bnext<CR>", "Próximo buffer")
+keymap("n", "<S-h>", ":bprevious<CR>", "Buffer anterior")
 -- Fechar buffer
 keymap("n", "<leader>x", ":bd<CR>", "Fechar buffer")
 
+--[[ GERENCIAMENTO DE JANELAS ]]
 -- Dividir janela
 keymap("n", "<leader>sv", ":vsplit<CR>", "Dividir verticalmente")
 keymap("n", "<leader>sh", ":split<CR>", "Dividir horizontalmente")
@@ -29,11 +39,14 @@ keymap("n", "<C-j>", "<C-w>j", "Janela abaixo")
 keymap("n", "<C-k>", "<C-w>k", "Janela acima")
 keymap("n", "<C-l>", "<C-w>l", "Janela à direita")
 
--- LSP Renomeação (estilo VSCode)
+--[[ LSP E CÓDIGO ]]
+-- Renomeação e Referências
+
 -- F2: Renomeia o símbolo sob o cursor
 keymap("n", "<F2>", vim.lsp.buf.rename, "Renomear símbolo")
 -- Ctrl+F2: Mostra todas as referências do símbolo (usando Telescope para melhor visualização)
 --keymap("n", "<C-F2>", vim.lsp.buf.references, "Mostrar todas as referências")
+keymap("n", "<leader>rn", vim.lsp.buf.rename, "Renomear símbolo (alt F2)")
 
 -- Definição e Implementação
 --[[ Keymaps LSP padrão (usando quickfix list)
@@ -42,28 +55,27 @@ keymap("n", "gi", vim.lsp.buf.implementation, "Ir para implementação")
 keymap("n", "gr", vim.lsp.buf.references, "Encontrar referências")
 ]]
 
--- Keymaps LSP com Telescope (interface mais amigável)
+-- Navegação de código com Telescope
 keymap("n", "gd", "<cmd>Telescope lsp_definitions<CR>", "Ir para definição")
 keymap("n", "gi", "<cmd>Telescope lsp_implementations<CR>", "Ir para implementação")
 keymap("n", "gr", "<cmd>Telescope lsp_references<CR>", "Encontrar referências")
 keymap("n", "K", vim.lsp.buf.hover, "Mostrar documentação")
 
--- Ações de código e diagnósticos
+-- Ações e diagnósticos
 keymap("n", "<leader>ca", vim.lsp.buf.code_action, "Ações de código")
--- Alternativa ao F2 para renomear símbolos (útil quando F2 não está disponível no terminal)
-keymap("n", "<leader>rn", vim.lsp.buf.rename, "Renomear símbolo (alt F2)")
 keymap("n", "[d", vim.diagnostic.goto_prev, "Diagnóstico anterior")
 keymap("n", "]d", vim.diagnostic.goto_next, "Próximo diagnóstico")
 keymap("n", "<leader>d", vim.diagnostic.open_float, "Mostrar diagnóstico")
 keymap("n", "<leader>q", vim.diagnostic.setloclist, "Lista de diagnósticos")
 
--- Workspace
+--[[ WORKSPACE ]]
 keymap("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, "Adicionar pasta ao workspace")
 keymap("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, "Remover pasta do workspace")
 keymap("n", "<leader>wl", function()
   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 end, "Listar pastas do workspace")
 
+--[[ MOVIMENTAÇÃO DE LINHAS ]]
 --[[
 Movendo a linha selecionada ↑↓
 Alt - j Move a linha do cursor pra cima
@@ -72,28 +84,26 @@ Alt - k Move a linha do cursor pra baixo
 Também é possivel mover as linhas selecionadas
 ]]
 
+-- Mover linha/seleção para baixo
 keymap("n", "<A-j>", ":m .+1<CR>==")
 keymap("n", "<A-Down>", ":m .+1<CR>==")
-
 keymap("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
 keymap("i", "<A-Down>", "<Esc>:m .+1<CR>==gi")
-
 keymap("v", "<A-j>", ":m '>+1<CR>gv=gv")
 keymap("v", "<A-Down>", ":m '>+1<CR>gv=gv")
-
 keymap("x", "<A-j>", ":m'>+<CR>gv=gv")
 keymap("x", "<A-Down>", ":m'>+<CR>gv=gv")
 
+-- Mover linha/seleção para cima
 keymap("n", "<A-k>", ":m .-2<CR>==")
 keymap("n", "<A-Up>", ":m .-2<CR>==")
-
 keymap("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
 keymap("i", "<A-Up>", "<Esc>:m .-2<CR>==gi")
-
 keymap("v", "<A-k>", ":m '<-2<CR>gv=gv")
 keymap("v", "<A-Up>", ":m '<-2<CR>gv=gv")
-
 keymap("x", "<A-Up>", ":m-2<CR>gv=gv")
 keymap("x", "<A-k>", ":m-2<CR>gv=gv")
 
+--[[ PLUGINS ]]
+-- Kulala (HTTP Client)
 keymap("n", "<C-j>", ":lua require('kulala').run()<CR>")
