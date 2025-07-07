b99feff0f729d0cd240fa88cea4200867f6efa1b
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2025-07-06 23:51:46 -0300

    📦️ chore: add ultimate-autopairs
    agora quando apertar qualquer tecla que tenha um par, o par será
    adicionado automaticamente
    ex:
    press `(` e automaticamente o par será adicionado `)`

---
+++ lua/plugins/ultimate-autopairs.lua
@@ -0,0 +1,9 @@
+-- link: https://github.com/altermo/ultimate-autopair.nvim
+return {
+  "altermo/ultimate-autopair.nvim",
+  event = { "InsertEnter", "CmdlineEnter" },
+  branch = "v0.6", --recommended as each new version will have breaking changes
+  opts = {
+    --Config goes here
+  },
+}
