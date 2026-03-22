31468e1688e8bec7ff95dc61c1d1c8a09a1ab43b
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2026-03-21 21:40:38 -0300

    feat: remove deprecated package and install another markdown-preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,44 +1,14 @@
--- install without yarn or npm
---[[{
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-}]]
-
-return
--- install with yarn or npm
-{
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install",
-  init = function()
-    vim.g.mkdp_open_ip = "127.0.0.1"
-    vim.g.mkdp_port = "8888"
-
-    vim.g.mkdp_filetypes = { "markdown" }
-    vim.g.mkdp_refresh_slow = 0
-
-    vim.g.mkdp_auto_start = 1
-
-    vim.g.mkdp_auto_close = 1
-
-    vim.g.mkdp_page_title = "「${name}」"
-
-    vim.g.mkdp_preview_options = {
-      mkit = {},
-      katex = {},
-      uml = {},
-      maid = {},
-      disable_sync_scroll = 0,
-      sync_scroll_type = "middle",
-      hide_yaml_meta = 1,
-      sequence_diagrams = {},
-      flowchart_diagrams = {},
-      content_editable = false,
-      disable_filename = 0,
-      toc = {},
-    }
+return {
+  "selimacerbas/markdown-preview.nvim",
+  dependencies = { "selimacerbas/live-server.nvim" },
+  config = function()
+    require("markdown_preview").setup({
+      -- all optional; sane defaults shown
+      instance_mode = "takeover", -- "takeover" (one tab) or "multi" (tab per instance)
+      port = 0, -- 0 = auto (8421 for takeover, OS-assigned for multi)
+      open_browser = true,
+      debounce_ms = 300,
+      scroll_sync = true
+    })
   end,
-  ft = { "markdown" },
 }
