2aec836072e51b920fbeae9f0767d946e5bd296c
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2026-03-23 10:55:52 -0300

    ✨ feat: migrate from nvim-cmp to blink.cmp
    using blink.compat for:
      - cmp-calc
      - cmp-latex-symbols
      - cmp-sql

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,148 +1,69 @@
 return {
-  "hrsh7th/nvim-cmp",
+  "saghen/blink.cmp",
   dependencies = {
+    "rafamadriz/friendly-snippets",
+    { "saghen/blink.compat", opts = {} },
     "hrsh7th/cmp-calc",
     "kdheepak/cmp-latex-symbols",
     "ray-x/cmp-sql",
-
-    -- Snippets Engine
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-
-    -- Snippets
-    "rafamadriz/friendly-snippets",
-
-    -- Adds vscode-like pictograms
-    "onsails/lspkind.nvim",
-
-    -- Adds LSP completion capabilities
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-path",
   },
-
-  config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-
-    local kind_icons = {
-      Text = "",
-      Method = "󰆧",
-      Function = "󰊕",
-      Constructor = "",
-      Field = "󰇽",
-      Variable = "󰂡",
-      Class = "󰠱",
-      Interface = "",
-      Module = "",
-      Property = "󰜢",
-      Unit = "",
-      Value = "󰎠",
-      Enum = "",
-      Keyword = "󰌋",
-      Snippet = "",
-      Color = "󰏘",
-      File = "󰈙",
-      Reference = "",
-      Folder = "󰉋",
-      EnumMember = "",
-      Constant = "󰏿",
-      Struct = "",
-      Event = "",
-      Operator = "󰆕",
-      TypeParameter = "󰅲",
-    }
-
-    require("luasnip.loaders.from_vscode").lazy_load()
-    luasnip.config.setup({})
-
-    local base_sources = {
-      { name = "nvim_lsp" },
-      { name = "luasnip" },
-      { name = "path" },
-      { name = "calc" },
-      { name = "buffer" },
-    }
-
-    local base_config = {
-      snippet = {
-        expand = function(args)
-          require("luasnip").lsp_expand(args.body)
-        end,
+  version = "*",
+  opts = {
+    keymap = {
+      preset = "default",
+      ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+      ["<CR>"] = { "accept", "fallback" },
+      ["<C-Space>"] = { "show", "fallback" },
+      ["<C-e>"] = { "hide", "fallback" },
+      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+    },
+    appearance = {
+      nerd_font_variant = "mono",
+    },
+    snippets = {
+      preset = "default",
+    },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer", "calc" },
+      per_filetype = {
+        sql = { "lsp", "path", "snippets", "buffer", "sql" },
+        mysql = { "lsp", "path", "snippets", "buffer", "sql" },
+        plsql = { "lsp", "path", "snippets", "buffer", "sql" },
+        tex = { "lsp", "path", "snippets", "buffer", "latex_symbols" },
+        latex = { "lsp", "path", "snippets", "buffer", "latex_symbols" },
+        plaintex = { "lsp", "path", "snippets", "buffer", "latex_symbols" },
       },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
+      providers = {
+        calc = {
+          name = "calc",
+          module = "blink.compat.source",
+        },
+        sql = {
+          name = "sql",
+          module = "blink.compat.source",
+        },
+        latex_symbols = {
+          name = "latex_symbols",
+          module = "blink.compat.source",
+          opts = { strategy = 0 },
+        },
       },
-      completion = {
-        completeopt = "menu,menuone,noinsert",
+    },
+    completion = {
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+        window = { border = "rounded" },
       },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      }),
-      formatting = {
-        format = function(entry, vim_item)
-          local lspkind_ok, lspkind = pcall(require, "lspkind")
-          if not lspkind_ok then
-            vim_item.kind =
-              string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
-            vim_item.menu = ({
-              nvim_lsp = "[LSP]",
-              luasnip = "[LuaSnip]",
-              buffer = "[Buffer]",
-              latex_symbols = "[LaTeX]",
-              calc = "[Calc]",
-              sql = "[SQL]",
-            })[entry.source.name]
-            return vim_item
-          else
-            return lspkind.cmp_format()(entry, vim_item)
-          end
-        end,
+      menu = {
+        border = "rounded",
       },
-      sources = cmp.config.sources(base_sources),
-    }
-
-    cmp.setup(base_config)
-
-    -- SQL: adiciona keywords SQL
-    cmp.setup.filetype({ "sql", "mysql", "plsql" }, {
-      sources = cmp.config.sources(vim.list_extend(
-        vim.deepcopy(base_sources),
-        { { name = "sql" } }
-      )),
-    })
-
-    -- LaTeX: adiciona símbolos LaTeX
-    cmp.setup.filetype({ "tex", "latex", "plaintex" }, {
-      sources = cmp.config.sources(vim.list_extend(
-        vim.deepcopy(base_sources),
-        { { name = "latex_symbols", option = { strategy = 0 } } }
-      )),
-    })
-  end,
+    },
+    signature = {
+      enabled = true,
+      window = { border = "rounded" },
+    },
+  },
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,11 +19,8 @@ return {
       },
     })
 
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
     vim.lsp.config("*", {
-      capabilities = capabilities,
+      capabilities = require("blink.cmp").get_lsp_capabilities(),
     })
 
     vim.api.nvim_create_autocmd("LspAttach", {
